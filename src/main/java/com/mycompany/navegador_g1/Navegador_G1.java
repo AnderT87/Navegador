152cbba5513c5bf02e275f0fe5cdc860109c7d9f
AnderT87 <anderayov@gmail.com>
2025-10-22 23:14:29 -0500

    Navegador 1.0

---
+++ src/main/java/com/mycompany/navegador_g1/Navegador_G1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.navegador_g1;
+
+/**
+ *
+ * @author Calavera
+ */
+public class Navegador_G1 {
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+}
---
+++ src/main/java/com/mycompany/navegador_g1/Pagina.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.navegador_g1;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ *
+ * @author Calavera
+ */
+public class Pagina {
+
+    private String actual;
+    private Stack<String> adelante;
+    private Stack<String> atras;
+    private ArrayList<String> visitar;
+
+    public Pagina(String actual) {
+        this.actual = actual;
+        adelante = new Stack<>();
+        atras = new Stack<>();
+        visitar = new ArrayList<>();
+        visitar.add(actual);
+    }
+
+    public void visitarPagnina(String url) {
+        if(actual != null){
+            atras.push(actual);
+        }
+        actual = url;
+        adelante.clear();
+        visitar.add(url);
+        System.out.println("Te encuentras en:"+url+"/n");
+    }
+
+}
